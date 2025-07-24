8f34d69465487603d605f67529438f548c83e63f
Daniel <daniel.w.garcia@gmail.com>
2025-07-24 15:44:24 -0700

    Completed hash lesson and exercises

---
+++ BasicSyntax.rb
@@ -0,0 +1,73 @@
+﻿print "Learning to code is FUN!"
+
+print "1234"
+
+puts "Learning to code is FUN!"
+puts "1234"
+
+5 <=> 10    #=> -1
+10 <=> 10   #=> 0
+10 <=> 5    #=> 1
+
+i = 0
+
+loop do
+  puts "i is #{i}"
+  i+= 1
+  break if i == 10
+end
+
+j = 0
+while j < 10
+  puts "j is #{j}"
+  j += 1
+end
+
+k = 0
+until k >=10 do
+  puts "k is #{k}"
+  k += 1
+end
+
+for a in 0..5 # two dots = inclusive list
+  puts "#{a} zombies incomming"
+end
+
+for b in 0...5 # this doens't include 5
+  puts "#{b} zombies incomming"
+end
+
+5.times do
+  puts "Hello party people"
+end
+
+5.upto(10){|num| print "#{num}" }
+puts ""
+10.downto(1){|num| print "#{num}"}
+
+puts""
+
+
+for c in 0...20
+  if c.odd?
+  puts c
+  end
+end
+
+puts""
+
+x = 0
+
+while x <= 10
+  if x == 3
+    x += 1
+    next
+  elsif x.odd?
+    puts x
+  end
+  x += 1
+end
+
+names = ["Dan", "Mike", "Joe"]
+
+names.each {|name| puts name } # using the 2 pipes around name is how we declare a var to store 'puts' results
---
+++ hashes.rb
@@ -0,0 +1,62 @@
+﻿# frozen_string_literal: true
+
+class Hashes
+
+  my_hash = {
+    "a random word" => "ahoy",
+    "Dorothy's math test score" => 94,
+    "an array" => [1, 2, 3],
+    "an empty hash within a hash" => {}
+  }
+  my_hash1 = Hash.new
+  my_hash1               #=> {}
+
+  hash = { 9 => "nine", :six => 6 } # a ':' colon in Ruby is used to create symbols
+
+  shoes = {
+    "summer" => "sandals",
+    "winter" => "boots"
+  }
+
+  shoes["summer"]   #=> "sandals"
+  shoes["hiking"]   #=> nil "hiking isn't in the hash so we get nil here
+  shoes.fetch("hiking")   #=> KeyError: key not found: "hiking"
+
+  shoes["fall"] = "sneakers"
+
+  shoes     #=> {"summer"=>"sandals", "winter"=>"boots", "fall"=>"sneakers"}
+
+  shoes["summer"] = "flip-flops" #changing the value in key 'summer' from sandals to flip flops
+  shoes     #=> {"summer"=>"flip-flops", "winter"=>"boots", "fall"=>"sneakers"}
+
+  books = {
+    "Infinite Jest" => "David Foster Wallace",
+    "Into the Wild" => "Jon Krakauer"
+  }
+
+  books.keys      #=> ["Infinite Jest", "Into the Wild"] 'keys' method returns keys
+  books.values    #=> ["David Foster Wallace", "Jon Krakauer"] 'values' method returns values
+
+  hash1 = { "a" => 100, "b" => 200 } # hash rocket used to make 2 KVP's
+  hash2 = { "b" => 254, "c" => 300 }
+  hash1.merge(hash2)      #=> { "a" => 100, "b" => 254, "c" => 300 } 'merge method used
+
+  # 'Rocket' syntax
+  american_cars = {
+    :chevrolet => "Corvette",
+    :ford => "Mustang",
+    :dodge => "Ram"
+  }
+  # 'Symbols' syntax ONLY WORKS WITH SYMBOLS AS THE KEYS
+  japanese_cars = {
+    honda: "Accord",
+    toyota: "Corolla",
+    nissan: "Altima"
+  }
+
+  american_cars[:ford]    #=> "Mustang"
+  japanese_cars[:honda]   #=> "Accord"
+
+
+
+end
