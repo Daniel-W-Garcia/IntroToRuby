9f1109e6ad87acfca933f7a4b7b661e3aa6f72d1
Daniel <daniel.w.garcia@gmail.com>
2025-07-25 16:22:12 -0700

    Methods lessons finished

--- Methods.rb
+++ Methods.rb
@@ -69,4 +69,19 @@ whisper1 = "HELLO EVERYBODY"
 puts whisper1.downcase! #=> "hello everybody" adding the bang or '!' will overwrites the original value
 puts whisper1 #=> "hello everybody"
 
+"hi there".length.to_s      # returns "8" - a String
+
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+multiply(add(20, 45), subtract(80, 10)) # this is crazy to me. I haven't really seen it in C#
 
--- Return.rb
+++ Return.rb
@@ -1,2 +1,14 @@
 ﻿# frozen_string_literal: true
 
+def add_three(number)
+  number + 3
+end
+
+returned_value = add_three(5)
+puts returned_value
+
+def add_four(number)
+  number + 4
+end
+
+add_four(5).times { puts "Hello" } #=> Hello 9 times AND returns '9'
